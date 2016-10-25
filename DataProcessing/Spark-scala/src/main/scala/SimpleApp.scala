4e47d62f97299c2d6fd8410732906172722a72a3
neranjan edirisinghe <neranjan@gsu.edu>
2016-10-25 15:27:56 -0400

    Adding some comments

--- DataProcessing/Spark-scala/src/main/scala/SimpleApp.scala
+++ DataProcessing/Spark-scala/src/main/scala/SimpleApp.scala
@@ -18,11 +18,32 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
 object SimpleApp {
 def main(args: Array[String]) {
- val conf = new SparkConf().setAppName("Simple Application")
- val sc = new SparkContext(conf)
- val sqlContext = new SQLContext(sc)
- val df = sqlContext.read.format("com.databricks.spark.csv").option("header","true").option("inferSchema","true").load("hdfs://dicenode02.rs.gsu.edu:8020/user/appadmin/Eventbrite/Data.csv")
- val Results = df.groupBy("category").count()
- Results.write.format("com.databricks.spark.csv").save("hdfs://dicenode02.rs.gsu.edu:8020/user/appadmin/Eventbrite/Result.csv")
+
+//Set application name 
+
+val conf = new SparkConf().setAppName("Simple Application")
+
+//we create new sparkcontext and sql context
+
+val sc = new SparkContext(conf)
+val sqlContext = new SQLContext(sc)
+
+//here we read data from the distributed file located at HDFS file system /user/appadmin/Eventbrite/
+//dicenode02.rs.gsu.edu:8020 is the entry point to your hadoop cluster
+//option("header","true") intruct driver that the headers are present in the csv file
+//option("inferSchema","true") instruct the databrick csv driver to infer the schema from the csv file itself
+
+val df = sqlContext.read.format("com.databricks.spark.csv").option("header","true").option("inferSchema","true").load("hdfs://dicenode02.rs.gsu.edu:8020/user/appadmin/Eventbrite/Data.csv")
+
+// The about command create a data fream df, now we can operate SQL like operations on the data fream
+//http://spark.apache.org/docs/latest/sql-programming-guide.html
+//Here we count different category and lable them 
+
+val Results = df.groupBy("category").count()
+
+//Now save results back to HDFS so that we can retrieve it later
+
+Results.write.format("com.databricks.spark.csv").save("hdfs://dicenode02.rs.gsu.edu:8020/user/appadmin/Eventbrite/Result.csv")
+
 }
 }
